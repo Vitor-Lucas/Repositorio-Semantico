5b97e55857f15cdc2c2609b438daaccd622d4340
V! <vitorlucaskc@gmail.com>
2025-10-28 00:51:22 -0300

    Todos os códigos de ICAs funcionando

--- ICA_Extractor/ICAExtractor.py
+++ ICA_Extractor/ICAExtractor.py
@@ -1,21 +1,22 @@
-import sys
-import time
+import os
 
-from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import psutil
-import urllib.request
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium import webdriver
 from bs4 import BeautifulSoup
+import urllib.request
 import pandas as pd
 import requests
+import psutil
+import utils
 
 
 class ICAExtrator:
-    def __init__(self):
-        self.active_icas_file_path = "icas.csv"
+    def __init__(self, icas_dataframe_path: str, icas_download_dir: str):
+        self.icas_dataframe_path = icas_dataframe_path
+        self.icas_download_dir = icas_download_dir
 
     def create_ica_dataframe(self):
         print('-'*40)
@@ -50,24 +51,25 @@ class ICAExtrator:
         print('Estrutura do dataframe criado!')
         print(f'Quantidade de linhas no dataframe: {len(df_schema["numero"])}')
         dataframe = pd.DataFrame(df_schema)
-        dataframe.to_csv(self.active_icas_file_path, index=False)
-        print(f'Dataframe criado no caminho "{self.active_icas_file_path}"')
+        dataframe.to_csv(self.icas_dataframe_path, index=False)
+        print(f'Dataframe criado no caminho "{self.icas_dataframe_path}"')
         print('-'*40)
 
     def get_active_icas(self) -> list:
-        with open(self.active_icas_file_path, 'r', encoding='utf-8') as file:
+        with open(self.icas_dataframe_path, 'r', encoding='utf-8') as file:
             return [line.split(',') for line in file.readlines()[1:]]
 
     def download_ica_documents(self):
         print('-'*40)
         print('INICIANDO PROCESSO DE DOWNLOAD DOS ICAS')
         icas = self.get_active_icas()
-        print(f'ICAs ativos obtidos pelo caminho {self.active_icas_file_path}')
+        print(f'ICAs ativos obtidos pelo caminho {self.icas_dataframe_path}')
 
         print('Criando a instancia do webdriver')
         # chromedriver_path = r"C:\Users\vitor\Downloads\chromedriver_win32\chromedriver.exe"
-        # service = Service(executable_path=chromedriver_path)
-        driver = webdriver.Chrome()
+        chromedriver_path = os.path.join(os.getcwd(), "ICA_Extractor", "chromedriver.exe")
+        service = Service(executable_path=chromedriver_path)
+        driver = webdriver.Chrome(service=service)
         p = psutil.Process(driver.service.process.pid)
         print('Webdriver criado!')
 
@@ -101,7 +103,7 @@ class ICAExtrator:
                     if td.find('a'):
                         link = td.find('a').get('href')
                         print(f"Link do documento: {link}")
-                        download_file(link, numero)
+                        self.download_file(link, numero)
                     # print(f'{td.getText()}', end='| ')
                 print()
             print('-'*20)
@@ -112,26 +114,16 @@ class ICAExtrator:
         print('driver e processo fechados')
         print('-'*40)
 
+    def download_file(self, url: str, file_name: str):
+        response = requests.get(url)
+        file_path = os.path.join(self.icas_download_dir, file_name + ".pdf")
 
-def download_file(url: str, file_name: str):
-    # nome_arquivo = url.split('/')[-1]
-    response = requests.get(url)
-    file_path = "ICAS/"+file_name + ".pdf"
-
-    # if response.status_code == 200:
-    #     with open(file_path, "wb") as f:
-    #         f.write(response.content)
-    #     print("✅ Arquivo baixado com sucesso!")
-    #     print(f"Caminho do arquivo: {file_path}")
-    # else:
-    #     print(f"❌ Erro ao baixar o Arquivo: {response.status_code}")
-
-    if response.status_code == 200:
-        try:
-            urllib.request.urlretrieve(url, file_path)
-            print(f"✅ PDF baixado com urllib: {file_path}")
-        except Exception as e:
-            print(f"❌ Erro ao baixar o PDF: {e}")
+        if response.status_code == 200:
+            try:
+                urllib.request.urlretrieve(url, file_path)
+                print(f"✅ PDF baixado com urllib: {file_path}")
+            except Exception as e:
+                print(f"❌ Erro ao baixar o PDF: {e}")
 
 
 def get_html_from_url(url: str) -> str:
@@ -143,10 +135,14 @@ def get_html_from_url(url: str) -> str:
 
 
 if __name__ == '__main__':
-    # response = requests.get(r"https://publicacoes.decea.mil.br/api/lazy/history?id=1")
-    # print(response.status_code)
-    # print(response.text)
-    # sys.exit()
-    i = ICAExtrator()
-    # i.create_ica_dataframe()
-    i.download_ica_documents()
+    utils.garantir_cwd_para("Repositorio-Semantico")
+
+    dataframe_path = os.path.join(os.getcwd(), "ICA_Extractor", "icas.csv")
+    icas_path = os.path.join(os.getcwd(), "ICA_Extractor", "ICAS")
+    extrator = ICAExtrator(
+        icas_dataframe_path=dataframe_path,
+        icas_download_dir=icas_path
+    )
+
+    # extrator.create_ica_dataframe()
+    extrator.download_ica_documents()
